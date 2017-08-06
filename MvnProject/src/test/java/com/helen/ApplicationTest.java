5e55d1726cbbffabc3a9ee2068705cd7c2d2c177
helen <helen@192.168.100.103>
2017-08-06 17:46:51 +0900

    add unit test

---
+++ MvnProject/src/test/java/com/helen/ApplicationTest.java
@@ -0,0 +1,38 @@
+package com.helen;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.helen.boot.Application;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+@WebIntegrationTest()
+public class ApplicationTest {
+	private MockMvc mvc;
+	
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new Application()).build();
+	}
+
+	@Test
+	public void test() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/")
+				.accept(MediaType.TEXT_PLAIN))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content()
+						.string(Matchers.equalTo("Hello World")));
+	}
+}
