51a7b4470bce41502c768ddf93206323fdecd5dd
helen <helen@192.168.100.101>
2017-09-11 20:32:59 +0900

    use thymeleaf to show YourKit

--- spring-boot-jdbc/src/main/java/com/helen/boot/SpringBootJdbcApplication.java
+++ spring-boot-jdbc/src/main/java/com/helen/boot/SpringBootJdbcApplication.java
@@ -7,19 +7,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+//@RestController
+@Controller
 public class SpringBootJdbcApplication {
 	
 	@Autowired
 	private JdbcTemplate template;
 
+	// access the url : http://localhost:8080/stocks
 	@RequestMapping("/stocks")
-	public List<Map<String, Object>> stocks() {
-		return template.queryForList("select * from stock");
+	public String stocks(Model model) {
+		List<Map<String, Object>> result = template.queryForList("select * from stock_helen");
+		model.addAttribute("stocks", result);
+		return "index";
 	}
 
 	public static void main(String[] args) {
