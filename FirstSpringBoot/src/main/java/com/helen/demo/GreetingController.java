805b73ca5233a408516f6bdfe2d5b8d9458a1644
helen <helen@192.168.100.101>
2017-09-11 19:57:05 +0900

    make thymeleaf work

--- FirstSpringBoot/src/main/java/com/helen/demo/GreetingController.java
+++ FirstSpringBoot/src/main/java/com/helen/demo/GreetingController.java
@@ -1,12 +1,12 @@
 package com.helen.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class GreetingController {
 
     private static final String HELLO_VALUE = "Hello , ";
