fedc2fcb4a865dafd4cf329b1a975e17c5f69893
helen <helen@192.168.100.102>
2017-12-11 21:21:10 +0900

    use cache manager

--- spring-boot-cache/src/main/java/com/helen/boot/SpringBootCacheApplication.java
+++ spring-boot-cache/src/main/java/com/helen/boot/SpringBootCacheApplication.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringBootCacheApplication implements CommandLineRunner {
 
 	@Autowired
@@ -21,5 +23,8 @@ public class SpringBootCacheApplication implements CommandLineRunner {
 		for(int x = 0; x < 10; x++) {
 			System.out.println(tracker.getPrice("T"));
 		}
+
+		System.out.println("Getting price with new method");
+		System.out.println(tracker.getPriceWithManager("T"));
 	}
 }
--- spring-boot-cache/src/main/java/com/helen/boot/StockTracker.java
+++ spring-boot-cache/src/main/java/com/helen/boot/StockTracker.java
@@ -4,11 +4,24 @@ import java.util.Random;
 
 import javax.cache.annotation.CacheResult;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+
 
 @Component
 public class StockTracker {
-	@CacheResult
+	@Autowired
+	private CacheManager manager;
+
+	public double getPriceWithManager(String symbol) {
+		Cache cache = manager.getCache("price");
+		return Double.valueOf(cache.get(symbol).getObjectValue().toString());
+	}
+
+	@CacheResult(cacheName="price")
 	public double getPrice(String symbol) {
 		System.out.println("Generating the price...");
 		return 20 + (200 - 20) * new Random().nextDouble();
